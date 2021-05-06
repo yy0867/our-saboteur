cccbfa4f81477baf574ff692d3a75a102dc87975
yy0867 <yy0867@naver.com>
2021-05-06 15:00:36 +0900

    MainForm setting change

--- Saboteur/Saboteur/MainForm.Designer.cs
+++ Saboteur/Saboteur/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Saboteur
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
@@ -29,10 +29,18 @@ namespace Saboteur
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MainForm";
+            this.Text = "Saboteur";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Saboteur/Saboteur/MainForm.cs
+++ Saboteur/Saboteur/MainForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Saboteur
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnCreateRoom_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
