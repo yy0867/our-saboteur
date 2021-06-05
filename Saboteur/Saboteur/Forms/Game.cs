bb3cf8ab3c752ac16f3e6676319ba0deecba2664
tellang <tellang@daum.net>
2021-06-05 23:42:33 +0900

    omitted ='

--- Saboteur/Saboteur/Forms/Game.cs
+++ Saboteur/Saboteur/Forms/Game.cs
@@ -371,7 +371,7 @@ namespace Saboteur.Forms
 
                     int index = GetPlayerIndex(mouseLocation);
 
-                    if (index > playerNum)
+                    if (index >= playerNum)
                     {
                         MoveToStartPosition(selectedPic);
                     }
